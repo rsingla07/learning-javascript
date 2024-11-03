d04b5f50dfb856ad06d4e96d4600ffe8d1b26d0f
rsingla07 <riyasingla212@gmail.com>
2024-11-03 17:18:17 +0530

    added

---
+++ conditional in js assignment/10.js
@@ -0,0 +1,13 @@
+let age=23
+if(age<12){
+    console.log("child")
+}
+else if(age<18 && age>=12){
+    console.log("teenager")
+}
+else if(age>=18 && age<60){
+    console.log("adult")
+}
+else{
+    console.log("senior citizen")
+}
