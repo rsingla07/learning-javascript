f90d153c0447e714e99ec85209c4faf30c497049
rsingla07 <riyasingla212@gmail.com>
2024-11-03 16:09:00 +0530

    added

---
+++ conditional in js assignment/1.js
@@ -0,0 +1,7 @@
+let a=-2
+if(a<0){
+    console.log(-1*a)
+}
+else{
+    console.log(a)
+}
---
+++ conditional in js assignment/2.js
@@ -0,0 +1,14 @@
+let marks=89
+if( marks>=90 && marks<=100 ){
+    console.log("grade A");
+}
+else if( marks>=75 && marks<90 ){
+    console.log("grade B");
+}
+else if(marks>=50 && marks<75){
+    console.log("grade C");
+}
+else{
+    console.log("grade F");
+}
+
