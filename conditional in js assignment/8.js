a88355081f1eed724707902815123a2102163c65
rsingla07 <riyasingla212@gmail.com>
2024-11-03 17:10:42 +0530

    added

---
+++ conditional in js assignment/8.js
@@ -0,0 +1,10 @@
+let a=98
+if(a<=9 && a>=0){
+    console.log("one digit number")
+}
+else if(a<=99 && a>=10){
+    console.log("two digit number")
+}
+else{
+    console.log("three digit number")
+}
