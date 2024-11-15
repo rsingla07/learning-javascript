8b673d75a489db021d602778a94c5666c47d820c
rsingla07 <riyasingla212@gmail.com>
2024-11-15 19:45:42 +0530

    added code

---
+++ conditional in js assignment/12.js
@@ -0,0 +1,10 @@
+let items=6
+if(items>=1 && items<=3){
+    console.log("5% discount")
+}
+else if(items>=4 && items<=6){
+    console.log("10% discount")
+}
+else if(items>6){
+    console.log("15% discount")
+}
